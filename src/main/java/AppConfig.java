57f936bbde62155534ce31d31a298f2992d3c13e
soloAnoynmous <deepdarksolo@gmail.com>
2020-03-17 02:12:04 +0530

    Adding Singleton scope to our Bean

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,16 @@ import com.tutorial.repository.SpeakerRepository;
 import com.tutorial.repository.SpeakerRepositoryImpl;
 import com.tutorial.service.SpeakerService;
 import com.tutorial.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
         //service.setRepository(getSpeakerRepository());
