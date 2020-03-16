987883329e25e989cc4814939d2170a040972464
soloAnoynmous <deepdarksolo@gmail.com>
2020-03-17 01:28:05 +0530

    updated the Application class to make use of spring

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,16 @@
 import com.tutorial.service.SpeakerService;
 import com.tutorial.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //SpeakerService service = new SpeakerServiceImpl();
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+
         System.out.println(service.findAll().get(0).getFirstName());
         System.out.println(service.findAll().get(0).getLastName());
     }
